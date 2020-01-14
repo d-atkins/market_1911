29b38382ec5b76e42d0b84f6a843563bdd00951f
David Atkins <atki1080@gmail.com>
2020-01-14 09:42:04 -0700

    Add vendors_that_sell

--- test/market_test.rb
+++ test/market_test.rb
@@ -47,4 +47,13 @@ class MarketTest < Minitest::Test
 
     assert_equal expected, @market.vendor_names
   end
+
+  def test_it_can_get_vendors_that_sell_an_item
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
+    assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
 end
