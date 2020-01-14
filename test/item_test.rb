f4a09d3fd6ebe1295be9cf76256ac93b728f969f
David Atkins <atki1080@gmail.com>
2020-01-14 09:12:39 -0700

    Add attributes test

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,8 +4,16 @@ require './lib/item'
 
 class ItemTest < Minitest::Test
 
+  def setup
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
   def test_it_exists
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    assert_instance_of Item, item2
+    assert_instance_of Item, @item2
+  end
+
+  def test_it_has_attributes
+    assert_equal "Tomato", @item2.name
+    assert_equal "$0.50", @item2.price
   end
 end
