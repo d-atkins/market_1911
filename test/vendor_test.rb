dca5edd81453ee20aa9ca795693e1de11ba4598b
David Atkins <atki1080@gmail.com>
2020-01-14 09:27:08 -0700

    Update check_stock test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -20,8 +20,20 @@ class VendorTest < Minitest::Test
     assert_equal ({}), @vendor.inventory
   end
 
-  def test_check_stock_defaults_to_zero
+  def test_it_can_check_stock
     assert_equal 0, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 30)
+
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+
+    assert_equal 12, @vendor.check_stock(@item2)
   end
 
   def test_it_can_stock_items
