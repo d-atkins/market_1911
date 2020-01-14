dd70a1d77e75c008f5a65ecd7c6c6a947a9f6eb1
David Atkins <atki1080@gmail.com>
2020-01-14 09:39:09 -0700

    Add add_vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -30,4 +30,12 @@ class MarketTest < Minitest::Test
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
   end
+
+  def test_it_can_add_vendors
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+  end
 end
