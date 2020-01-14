156871e8d3fd4928d5da17718ca75c72299ae9bb
David Atkins <atki1080@gmail.com>
2020-01-14 09:18:31 -0700

    Add check_stock test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require './lib/item'
 
 class VendorTest < Minitest::Test
 
@@ -15,5 +16,12 @@ class VendorTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal ({}), @vendor.inventory
+  end
+
+  def test_check_stock_defaults_to_zero
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    
+    assert_equal 0, @vendor.check_stock(item1)
   end
 end
